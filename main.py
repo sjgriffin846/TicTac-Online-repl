3774306698b21d6bc52477ef9b15783322df9f9c
sjgriffin846 <sjgriffin846@gmail.com>
2020-09-28 00:14:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import math
+
+from random import randint
+
+
+game_board = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+def display_board(game_baord):
+    print(str(game_baord[0]))
+    print('Test 1')
+
+
+display_board(game_board)
+
+print('hello')
+print(randint(1, 200))
