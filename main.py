01a5071c1cd27ca1d93e8adaa2babd61d836403c
sjgriffin846 <sjgriffin846@gmail.com>
2020-09-29 23:05:46 +0000

    Added play_on function and modified display_board function

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
-import math
-
-from random import randint
-
-
 game_board = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 
 def display_board(game_baord):
-    print(str(game_baord[0]))
-    print('Test 1')
-
+    print(str(game_baord[0]) + '|'+ str(game_baord[1]) + '|'+ str(game_baord[2]))
+    print('-----')
+    print(str(game_baord[0]) + '|'+ str(game_baord[1]) + '|'+ str(game_baord[2]))
+    print('-----')
+    print(str(game_baord[0]) + '|'+ str(game_baord[1]) + '|'+ str(game_baord[2]))
 
 display_board(game_board)
 
-print('hello')
-print(randint(1, 100))
+
+def play_on():
+  control = 1
+  while control == 1:
+    result_yn = input('Do you want to play a game? *SAW* (Y/N)')
+
+    if result_yn == "Y":
+      print('Okay ghost')
+      print('function here that starts game?')
+      control = 2
+    
+    if result_yn == 'N':
+      print('Smart move')
+      control = 2
+      break
+    
+play_on()
