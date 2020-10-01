2c8379372d2a52fa87f78acf33a98951dc46e03d
sjgriffin846 <sjgriffin846@gmail.com>
2020-10-01 03:39:32 +0000

    Place Marker added

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-game_board = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+game_board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 
+print('Hello and Welcome to Tic Tac Toe Murder')
 
 def display_board(game_baord):
     print(str(game_baord[0]) + '|'+ str(game_baord[1]) + '|'+ str(game_baord[2]))
     print('-----')
-    print(str(game_baord[0]) + '|'+ str(game_baord[1]) + '|'+ str(game_baord[2]))
+    print(str(game_baord[3]) + '|'+ str(game_baord[4]) + '|'+ str(game_baord[5]))
     print('-----')
-    print(str(game_baord[0]) + '|'+ str(game_baord[1]) + '|'+ str(game_baord[2]))
+    print(str(game_baord[6]) + '|'+ str(game_baord[7]) + '|'+ str(game_baord[8]))
 
 display_board(game_board)
 
@@ -17,26 +18,34 @@ def play_on():
     result_yn = input('Do you want to play a game? *SAW* (Y/N)')
 
     if result_yn == "Y":
-      print('Okay ghost')
+      print('Okay...')
       return 'Y'
       control = 2
     
     if result_yn == 'N':
       print('Smart move')
       control = 2
+      return 'N'
       break
     
-y_n = play_on()
 
+y_n = play_on()
 
 def player_input(a):
   while a == 'Y':
     result = input('Please Select  position for your next move 1-9')
     if int(result) in range(1,10):
-      return result
+      return int(result)
       break
     else:
       pass
 
+selection = player_input(y_n)
 
-player_input(y_n)
+def place_marker(board,marker,position):
+  board[position] = marker
+  return board
+
+place_marker(game_board, 'X', selection)
+
+display_board(game_board)
